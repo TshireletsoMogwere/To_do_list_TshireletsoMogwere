234ede554e53b6ae43b6fd02febda36ec344270c
TshireletsoMogwere <tmogwere09@gmail.com>
2024-04-08 20:21:19 +0200

    added empty comments

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+//Fetch elements from HTML
 
+//Event listener for add button
+    //Get value of input field
+
+//Check if input field is not empty
+    //Create a new list item element
+
+//Append new item to the item list
+
+//Clear input field
+    //If input field is empty show alert
